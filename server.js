a34c30fa2e5364d7ed9a375b7ca6fa77cb65a45f
Caio Emannuel <caioemannueldiniz@gmail.com>
2024-02-15 10:37:35 -0300

    Criando as rotas

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const fileupload = require("express-fileupload");
 
+const apiRoutes = require("./src/routes");
+
 mongoose.connect(process.env.DATABASE, {});
 
 mongoose.Promise = global.Promise;
@@ -20,9 +22,7 @@ server.use(fileupload());
 
 server.use(express.static(__dirname + "/public"));
 
-server.get("/ping", (req, res) => {
-  res.json({ pong: true });
-});
+server.use("/", apiRoutes);
 
 server.listen(process.env.PORT, () => {
   console.log(`Rodando servidor: ${process.env.BASE}`);
